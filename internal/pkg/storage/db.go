cde129bd2f1356c35fc500b6af7a08f432771ce7
huoju <virushuo@gmail.com>
2021-02-20 18:54:23 -0500

    add read/write to db

--- internal/pkg/storage/db.go
+++ internal/pkg/storage/db.go
@@ -1,7 +1,12 @@
 package storage
 
 import (
+	"crypto/sha1"
 	"database/sql"
+	"fmt"
+	"github.com/huo-ju/fairshares/internal/pkg/poolapi"
+	"github.com/mattn/go-sqlite3"
+	"time"
 )
 
 type Storage struct {
@@ -13,8 +18,10 @@ func NewStorage(db *sql.DB) *Storage {
 }
 
 func (s *Storage) NewDatabase() error {
-	sqlStmt := `create table workershares (checksum TEXT primary key, address TEXT not null, workername TEXT not null, valid_shares INTEGER, stale_shares INTEGER, invalid_shares INTEGER, lastseen TIMESTAMP); 
+	sqlStmt := `
 	create table setting (version INTEGER); 
+	create table workershares (checksum TEXT primary key, address TEXT not null, workername TEXT not null, valid_shares INTEGER, stale_shares INTEGER, invalid_shares INTEGER, lastseen TIMESTAMP); 
+	create table addresses (checksum TEXT primary key, address TEXT, poolname TEXT, created_at TIMESTAMP);
 	`
 	_, err := s.db.Exec(sqlStmt)
 	if err == nil {
@@ -30,4 +37,54 @@ func (s *Storage) DatabaseVersion() int {
 		return 0
 	}
 	return dbVersion
+}
+
+func (s *Storage) RegAddress(address string, poolname string) error {
+
+	str := fmt.Sprintf("%s %s", address, poolname)
+	checksumstr := fmt.Sprintf("%x", sha1.Sum([]byte(str)))
+	stmt, err := s.db.Prepare("insert into addresses (checksum, address, poolname, created_at) values(?,?,?,?)")
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(checksumstr, address, poolname, time.Now())
+	return err
+}
+
+func (s *Storage) GetAddresses(poolname string) (error, []string) {
+	results := []string{}
+	rows, err := s.db.Query(`select address from addresses where poolname=$1`, poolname)
+	for rows.Next() {
+		var address string
+		err = rows.Scan(&address)
+		if err == nil {
+			results = append(results, address)
+
+		}
+	}
+	rows.Close()
+	return nil, results
+}
+
+func (s *Storage) SaveWorkerShares(address string, workers []*poolapi.FlexpoolWorker) int {
+	tx, err := s.db.Begin()
+	stmt, err := tx.Prepare("insert into workershares(checksum, address, workername, valid_shares, stale_shares, invalid_shares, lastseen) values(?,?,?,?,?,?,?)")
+	defer stmt.Close()
+	insertcount := 0
+	for _, worker := range workers {
+		str := fmt.Sprintf("%s %s %d %d %d %d", address, worker.Name, worker.ValidShares, worker.StaleShares, worker.InvalidShares, worker.LastSeen)
+		checksumstr := fmt.Sprintf("%x", sha1.Sum([]byte(str)))
+		_, err = stmt.Exec(checksumstr, address, worker.Name, worker.ValidShares, worker.StaleShares, worker.InvalidShares, worker.LastSeen)
+		if err != nil {
+			sqliteErr := err.(sqlite3.Error)
+			if sqliteErr.ExtendedCode == sqlite3.ErrConstraintPrimaryKey {
+				insertcount++
+				//data exist, it's ok
+			}
+		} else {
+			insertcount++
+		}
+	}
+	tx.Commit()
+	return insertcount
 }
